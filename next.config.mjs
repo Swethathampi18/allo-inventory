444370c73e93f28f9500b6cb9db4b9b655f6b7ce
Swethathampi18 <thampiswetha154@gmail.com>
2026-05-24 01:04:28 +0530

    fix: next config and client components

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  outputFileTracingExcludes: {
+    "*": [
+      "node_modules/@swc/core-linux-x64-gnu",
+      "node_modules/@swc/core-linux-x64-musl",
+    ],
+  },
 };
 
 export default nextConfig;
